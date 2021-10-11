2ed46bc31d31dd25c868bcea4ecf2c4b664474d0
D.Boy.Blade <ykw0104@163.com>
2021-10-11 15:49:54 +0800

    83 路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [
   /* -- 1. film--------------------------------------------------------------------------------------------------- */
   {
     path: "/film",
-    component: () => import("@/views/Film"),
+    component: () => import(/* webpackChunkName: "kw-film" */ "@/views/Film"),
     children: [
       {
         path: "",
@@ -13,25 +13,51 @@ const routes = [
       },
       {
         path: "nowplaying",
-        component: () => import("@/views/film/Nowplaying"),
+        component: () =>
+          import(
+            /* webpackChunkName: "kw-film-nowplaying" */ "@/views/film/Nowplaying"
+          ),
       },
       {
         path: "comingsoon",
-        component: () => import("@/views/film/Comingsoon"),
+        component: () =>
+          import(
+            /* webpackChunkName: "kw-film-comingsoon" */ "@/views/film/Comingsoon"
+          ),
       },
     ],
   },
   /* --2 .center--------------------------------------------------------------------------------------------------- */
-  { path: "/center", component: () => import("@/views/Center") },
+  {
+    path: "/center",
+    component: () =>
+      import(/* webpackChunkName: "kw-center" */ "@/views/Center"),
+  },
   /* --3. cinema--------------------------------------------------------------------------------------------------- */
-  { path: "/cinema", component: () => import("@/views/Cinema") },
+  {
+    path: "/cinema",
+    component: () =>
+      import(/* webpackChunkName: "kw-cinema" */ "@/views/Cinema"),
+  },
   /* --4. detail详情页--------------------------------------------------------------------------------------------------- */
-  { path: "/detail/:myid", component: () => import("@/views/Detail") },
+  {
+    path: "/detail/:myid",
+    component: () =>
+      import(/* webpackChunkName: "kw-detail" */ "@/views/Detail"),
+  },
   /* --5. 登录页--------------------------------------------------------------------------------------------------- */
-  { path: "/login", component: () => import("@/views/Login") },
+  {
+    path: "/login",
+    component: () => import(/* webpackChunkName: "kw-login" */ "@/views/Login"),
+  },
   /* -- 404页面--------------------------------------------------------------------------------------------------- */
-  { path: "/:pathMatch(.*)", component: () => import("@/views/NotFound") },
+  {
+    path: "/:pathMatch(.*)",
+    component: () =>
+      import(/* webpackChunkName: "kw-notfound" */ "@/views/NotFound"),
+  },
 ];
+``;
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -42,7 +68,7 @@ const router = createRouter({
 router.beforeEach((to, from) => {
   const auth = ["/center", "/order", "/money", "/card"]; // 需要验证token的组件
   if (auth.includes(to.fullPath)) {
-    console.log("验证token");
+    console.log("验证token, 没有token返回登录页面");
   }
   return;
 });
