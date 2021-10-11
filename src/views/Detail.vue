03ce375c3d3d58b12a49f104f0c67d356773d94a
D.Boy.Blade <ykw0104@163.com>
2021-10-11 13:41:41 +0800

    79

---
+++ src/views/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from "vue";
+
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+    /* ----------------------------------------------------------------------------------------------------- */
+    onMounted(() => {
+      console.log(route.params.myid);
+    });
+    /* --------------------------------------------------------------------------------------------------- */
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/film/Nowplaying.vue
+++ src/views/film/Nowplaying.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>nnnnnnnn</div>
+  <div>
+    <ul>
+      <li v-for="data in dataList" :key="data.id" @click="handleClick(data.id)">
+        {{ data.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   setup() {
-    return {};
+    const dataList = ref([
+      { id: 1, title: "夺冠" },
+      { id: 2, title: "姜子牙" },
+      { id: 3, title: "花木兰" },
+    ]);
+
+    const router = useRouter();
+    /* ----------------------------------------------------------------------------------------------------- */
+    const handleClick = (id) => {
+      router.push(`/detail/${id}`); // 跳转到详情页
+    };
+
+    return {
+      dataList,
+
+      handleClick,
+    };
   },
 });
 </script>
