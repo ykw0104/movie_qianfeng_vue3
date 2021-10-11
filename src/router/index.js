ef42ae0b5d30ffc351db0f312984f3ba7c56e0fd
D.Boy.Blade <ykw0104@163.com>
2021-10-11 10:35:13 +0800

    76

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
+  { path: "/", redirect: "/film" },
   { path: "/film", component: () => import("@/views/Film") },
   { path: "/center", component: () => import("@/views/Center") },
   { path: "/cinema", component: () => import("@/views/Cinema") },
+  // 处理404
+  { path: "/:pathMatch(.*)", component: () => import("@/views/NotFound") },
 ];
 
 const router = createRouter({
