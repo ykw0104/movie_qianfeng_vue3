f44a2bf4942a9ac46ca729481a65c57efc8e63d3
D.Boy.Blade <ykw0104@163.com>
2021-10-13 15:40:51 +0800

    ...

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,14 +1,24 @@
 <template>
-  <div></div>
+  <div class="fl-detail" v-if="filminfo">
+    <div
+      class="fl-img"
+      :style="{ backgroundImage: 'url(' + filminfo.poster + ')' }"
+    ></div>
+
+    <h2>{{ filminfo.name }} - {{ filminfo.filmType.name }}</h2>
+    <div>{{ filminfo.category }}</div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { useRoute } from "vue-router";
 import http from "@/utils/http";
 
 export default defineComponent({
   setup() {
+    const filminfo = ref(null); // 保存电影信息
+
     const route = useRoute();
     /* ----------------------------------------------------------------------------------------------------- */
     http({
@@ -18,13 +28,21 @@ export default defineComponent({
         "X-Host": "mall.film-ticket.film.info",
       },
     }).then((res) => {
-      console.log(res);
+      filminfo.value = res.data.data.film;
     });
 
     /* --------------------------------------------------------------------------------------------------- */
-    return {};
+    return {
+      filminfo,
+    };
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fl-img {
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+</style>
