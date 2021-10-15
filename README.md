44423f609e72f27b7f952921c6aec2d37b833bca
D.Boy.Blade <ykw0104@163.com>
2021-10-15 16:20:33 +0800

    99

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # movie_qianfeng
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+1. views/Cinema.vue 里引入了 better-scroll
--- src/views/Cinema.vue
+++ src/views/Cinema.vue
@@ -1,15 +1,64 @@
 <template>
-  <div>cinema</div>
+  <div class="cinema" ref="cinemaRef">
+    <ul>
+      <li v-for="data in cinemaList" :key="data.cinemaId">
+        <div>{{ data.name }}</div>
+        <div class="address">{{ data.address }}</div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted, nextTick } from "vue";
+import BetterScroll from "better-scroll";
+
+import http from "@/utils/http";
 
 export default defineComponent({
   setup() {
-    return {};
+    const cinemaList = ref([]);
+    const cinemaRef = ref(null);
+
+    onMounted(() => {
+      http({
+        method: "GET",
+        url: "/gateway?cityId=440300&ticketFlag=1&k=4268042",
+        headers: { "X-Host": "mall.film-ticket.cinema.list" },
+      }).then((res) => {
+        cinemaList.value = res.data.data.cinemas;
+
+        nextTick(() => {
+          new BetterScroll(cinemaRef.value, {
+            scrollbar: {
+              fade: true,
+            },
+          });
+        });
+      });
+    });
+
+    return {
+      cinemaList,
+      cinemaRef,
+    };
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cinema {
+  // position: relative; // 修正better-scroll的滚动条位置, 解决出现拉到最底部的bug
+  height: calc(100vh - 50px);
+  overflow: hidden;
+
+  li {
+    padding: 5px;
+
+    .address {
+      font-size: 12px;
+      color: #ccc;
+    }
+  }
+}
+</style>
